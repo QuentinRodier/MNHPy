da8f3eb86ee74d152896cfd1fedae42d2be35c26
Quentin Rodier <quentin.rodier@meteo.fr>
2022-06-01 18:05:05 +0200

    Quentin 01/06/2022: prepare pip packaging

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+MAINTAINER_EMAIL = 'mesonhsupport@obs-mip.fr'
+AUTHORS = 'Quentin Rodier & Meso-NH Team'
+
+setup(
+   name='MNHPy',
+   version='0.1.0',
+   description=('Python visulization tools for MesoNH atmospheric research model'),
+   author=AUTHORS,
+   author_email=MAINTAINER_EMAIL,
+   url='https://github.com/QuentinRodier/MNHPy',
+   classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: CeCILL-C Free Software License Agreement (CECILL-C)"],
+   packages=['MNHPy'],	#name of package (dir with the modules)
+   # external dependencies packages
+   install_requires=['numpy>=1.17.2', 'matplotlib>=3.1.1', 'cartopy>=0.17', 'scipy>=1.3.1', 'netCDF4>=1.4.2'],
+   python_requires=">=3.6",
+   package_dir={"": "src"},
+   )
