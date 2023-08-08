3a3985e70054451e587a41ac0b4f2441b855f79c
Quentin Rodier <quentin.rodier@meteo.fr>
2023-08-08 12:03:32 +0200

    Version 0.3.0 with MesoNH 5.6.0+

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ AUTHORS = 'Quentin Rodier & Meso-NH Team'
 
 setup(
    name='MNHPy',
-   version='0.2.0',
+   version='0.3.0',
    description=('Python visulization tools for MesoNH atmospheric research model'),
-   long_description=('Compatible with Meso-NH model version 5.5.1'),
+   long_description=('Compatible with Meso-NH model version 5.6.0+'),
    author=AUTHORS,
    author_email=MAINTAINER_EMAIL,
    url='https://github.com/QuentinRodier/MNHPy',
@@ -18,7 +18,7 @@ setup(
        "License :: CeCILL-C Free Software License Agreement (CECILL-C)"],
    packages=['MNHPy'],	#name of package (dir with the modules)
    # external dependencies packages
-   install_requires=['numpy>=1.17.2', 'matplotlib>=3.1.1', 'cartopy>=0.17', 'scipy>=1.3.1', 'netCDF4>=1.4.2'],
-   python_requires=">=3.6",
+   install_requires=['numpy>=1.21.5', 'matplotlib>=3.7.0', 'cartopy>=0.21.1', 'scipy>=1.10.1', 'netCDF4>=1.5.3'],
+   python_requires=">=3.8.10",
    package_dir={"": "src"},
    )
