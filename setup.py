9d7f1cb578cc18629f72a296ee83dfed2a1cf3ab
Quentin Rodier <quentin.rodier@meteo.fr>
2023-10-02 13:30:01 +0200

    Quentin 02/10/2023: fix matlplotlib3.8 bug by using 3.7.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ AUTHORS = 'Quentin Rodier & Meso-NH Team'
 
 setup(
    name='MNHPy',
-   version='0.3.0',
+   version='0.3.1',
    description=('Python visulization tools for MesoNH atmospheric research model'),
    long_description=('Compatible with Meso-NH model version 5.6.0+'),
    author=AUTHORS,
@@ -18,7 +18,7 @@ setup(
        "License :: CeCILL-C Free Software License Agreement (CECILL-C)"],
    packages=['MNHPy'],	#name of package (dir with the modules)
    # external dependencies packages
-   install_requires=['numpy>=1.21.5', 'matplotlib>=3.7.0', 'cartopy>=0.21.1', 'scipy>=1.10.1', 'netCDF4>=1.5.3'],
+   install_requires=['numpy>=1.21.5', 'matplotlib==3.7.0', 'cartopy>=0.21.1', 'scipy>=1.10.1', 'netCDF4>=1.5.3'],
    python_requires=">=3.8.10",
    package_dir={"": "src"},
    )
