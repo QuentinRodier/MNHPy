b4db26b294cd0c04add8080c18732a8d4354858e
Quentin Rodier <quentin.rodier@meteo.fr>
2024-08-09 17:21:01 +0200

    Quentin 09/08/2024: adapt for Ubuntu 24.04 and MNH 5.7.1

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ AUTHORS = 'Quentin Rodier & Meso-NH Team'
 
 setup(
    name='MNHPy',
-   version='0.3.1',
+   version='0.3.2',
    description=('Python visulization tools for MesoNH atmospheric research model'),
-   long_description=('Compatible with Meso-NH model version 5.6.0+'),
+   long_description=('Compatible with Meso-NH model version >=5.7.1'),
    author=AUTHORS,
    author_email=MAINTAINER_EMAIL,
    url='https://github.com/QuentinRodier/MNHPy',
@@ -18,7 +18,7 @@ setup(
        "License :: CeCILL-C Free Software License Agreement (CECILL-C)"],
    packages=['MNHPy'],	#name of package (dir with the modules)
    # external dependencies packages
-   install_requires=['numpy>=1.21.5', 'matplotlib==3.7.0', 'cartopy>=0.21.1', 'scipy>=1.10.1', 'netCDF4>=1.5.3'],
-   python_requires=">=3.8.10",
+   install_requires=['numpy>=1.26.4', 'matplotlib==3.9.1', 'cartopy>=0.21.1', 'scipy>=1.14.0', 'netCDF4>=1.7.1'],
+   python_requires=">=3.12.3",
    package_dir={"": "src"},
    )
